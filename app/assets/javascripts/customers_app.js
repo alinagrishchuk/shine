79ef1011de755927316fae1ce57f3a1d00e8002d
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-05-26 11:07:50 +0300

    add typing search for customers#index

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,11 +1,39 @@
 var app = angular.module('customers',[]);
 
-app.controller("CustomerSearchController",
-    [ "$scope", CustomerSearchController ]
-);
 
-var CustomerSearchController = function ($scope) {
-    $scope.search = function (searcTerm) {
-        $scope.searchFor = searcTerm;
+app.controller("CustomerSearchController", [
+  "$scope", "$http",
+    function($scope, $http) {
+      var page = 0;
+      $scope.customers = [];
+
+      $scope.previousPage = function () {
+        if(page > 0) {
+          page = page - 1;
+          $scope.search($scope.keywords);
+        }
+      };
+      $scope.nextPage = function () {
+        page = page + 1;
+        $scope.search($scope.keywords);
+      };
+
+      $scope.search = function(searchTerm) {
+        if (searchTerm.length < 3) {
+          return;
+        }
+        $scope.searchedFor = searchTerm;
+        $http.get( "/customers.json",
+                 { "params": { "keywords": searchTerm, "page": page }}
+        ).then(function(response) {
+            $scope.customers = response.data;
+        },function(response) {
+            alert("There was a problem: " +  response.status);
+        });
+      }
     }
-}
+]);
+
+
+
+
